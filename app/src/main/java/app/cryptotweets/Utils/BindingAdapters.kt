0262aa76b2d3e9428fef97e5d6a71ce7d41d9e10
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-03-21 23:48:01 -0700

    Feature: Timestamps

--- app/src/main/java/app/cryptotweets/Utils/BindingAdapters.kt
+++ app/src/main/java/app/cryptotweets/Utils/BindingAdapters.kt
@@ -2,11 +2,15 @@ package app.cryptotweets.Utils
 
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import app.cryptotweets.R
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import kotlinx.android.synthetic.main.tweet_cell.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 @BindingAdapter("imageUrl")
 fun ImageView.setImageUrl(url: String?) {
@@ -25,4 +29,13 @@ private fun ImageView.transformImage(it: RequestBuilder<Drawable>) {
         it.apply(GlideOptions.circleCropTransform())
     else if (this.id == mediaImage?.id)
         it.transform(RoundedCorners(MEDIA_RADIUS_INT))
+}
+
+@BindingAdapter("timestamp")
+fun TextView.setTimestamp(createdAt: String) {
+    val twitterFormat = context.getString(R.string.twitter_date_time_format)
+    val simpleDateFormat = SimpleDateFormat(twitterFormat, Locale.getDefault())
+    val date = simpleDateFormat.parse(createdAt)
+    simpleDateFormat.applyPattern(context.getString(R.string.cryptotweets_date_time_format))
+    this.text = simpleDateFormat.format(date)
 }
