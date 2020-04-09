971b6312047ea264a5a641d5a60b9bc447e31a43
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-04-08 21:10:38 -0700

    Refactor: Remove unnecessary dependency injection component accessor methods, improve build.gradle documentation

--- app/src/main/java/app/cryptotweets/dependencyInjection/Component.kt
+++ app/src/main/java/app/cryptotweets/dependencyInjection/Component.kt
@@ -1,24 +1,11 @@
 package app.cryptotweets.dependencyInjection
 
-import android.content.SharedPreferences
 import app.cryptotweets.feed.FeedFragment
-import app.cryptotweets.feed.FeedRepository
-import app.cryptotweets.feed.network.FeedService
-import app.cryptotweets.feed.room.FeedDao
 import dagger.Component
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [UtilsModule::class])
 interface AppComponent {
     fun inject(feedFragment: FeedFragment)
-
-    fun sharedPreferences(): SharedPreferences
-    fun okHttpClient(): OkHttpClient
-    fun retrofit(): Retrofit
-    fun FeedService(): FeedService
-    fun feedRepository(): FeedRepository
-    fun feedDao(): FeedDao
 }
