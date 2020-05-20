711cbd07f21ecb1eb5fcc0d2a0dc963aaedc441c
Adam Hurwitz <3938076+AdamSHurwitz@users.noreply.github.com>
2020-05-20 11:30:38 -0700

    Refactor: ReplaySubject to BehaviorSubject

--- app-rx/src/main/java/app/cryptotweets/feed/viewmodel/FeedViewState.kt
+++ app-rx/src/main/java/app/cryptotweets/feed/viewmodel/FeedViewState.kt
@@ -2,12 +2,12 @@ package app.cryptotweets.feed.viewmodel
 
 import androidx.paging.PagedList
 import app.cryptotweets.feed.models.Tweet
-import io.reactivex.rxjava3.subjects.ReplaySubject
+import io.reactivex.rxjava3.subjects.BehaviorSubject
 
 data class _FeedViewState(
-    val _feed: ReplaySubject<PagedList<Tweet>> = ReplaySubject.create()
+    val _feed: BehaviorSubject<PagedList<Tweet>> = BehaviorSubject.create()
 )
 
 data class FeedViewState(private val _viewState: _FeedViewState) {
-    val feed: ReplaySubject<PagedList<Tweet>> = _viewState._feed
+    val feed: BehaviorSubject<PagedList<Tweet>> = _viewState._feed
 }
